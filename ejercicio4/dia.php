05e9a80b0be7d256a99808dbbc92df67d49016bf
JuanAntonioPanchon <martin.panchon.juan.antonio@iescamas.es>
2024-09-22 14:29:03 +0200

    Add files via upload

---
+++ ejercicio4/dia.php
@@ -0,0 +1,31 @@
+<?php
+    $numero = $_GET["numero"];
+
+    switch ($numero) {
+        case "1":
+            echo "Lunes";
+            break;
+        case "2":
+            echo "Martes";
+            break;
+        case "3":
+            echo "Miércoles";
+            break;
+        case "4":
+            echo "Jueves";
+            break;
+        case "5":
+            echo "Viernes";
+            break;
+        case "6":
+            echo "Sábado";
+            break;
+        case "7":
+            echo "Domingo";
+            break;
+        
+        default:
+           echo "El número introducido no corresponde a ningún día de la semana";
+    }
+
+?>
