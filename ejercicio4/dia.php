3bd3eaf91e43cfe3b3ffcd715009627e287c465f
JuanAntonioPanchon <martin.panchon.juan.antonio@iescamas.es>
2024-10-03 09:01:36 +0200

    Add files via upload
    He modificado el ejercicio4 con la comprobación de que sean valores numéricos

--- ejercicio4/dia.php
+++ ejercicio4/dia.php
@@ -1,31 +1,35 @@
 <?php
     $numero = $_GET["numero"];
 
-    switch ($numero) {
-        case "1":
-            echo "Lunes";
-            break;
-        case "2":
-            echo "Martes";
-            break;
-        case "3":
-            echo "Miércoles";
-            break;
-        case "4":
-            echo "Jueves";
-            break;
-        case "5":
-            echo "Viernes";
-            break;
-        case "6":
-            echo "Sábado";
-            break;
-        case "7":
-            echo "Domingo";
-            break;
-        
-        default:
-           echo "El número introducido no corresponde a ningún día de la semana";
+    if(isset($numero)&& is_numeric($numero)){
+        switch ($numero) {
+            case "1":
+                echo "Lunes";
+                break;
+            case "2":
+                echo "Martes";
+                break;
+            case "3":
+                echo "Miércoles";
+                break;
+            case "4":
+                echo "Jueves";
+                break;
+            case "5":
+                echo "Viernes";
+                break;
+            case "6":
+                echo "Sábado";
+                break;
+            case "7":
+                echo "Domingo";
+                break;
+            
+            default:
+               echo "El número introducido no corresponde a ningún día de la semana";
+        }
+    }else{
+        print("Debes introducir un valor numérico");
     }
 
 ?>
