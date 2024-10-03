fa84561730801d124ac918112f70113fb0201c27
JuanAntonioPanchon <martin.panchon.juan.antonio@iescamas.es>
2024-10-03 09:12:50 +0200

    Add files via upload
    comprobamos que la variable número no sea null

--- ejercicio6/acumulacion.php
+++ ejercicio6/acumulacion.php
@@ -1,10 +1,16 @@
 <?php
     $numero = $_GET["numero"];
     $suma = 0;
-    for ($i = 1; $i <= $numero; $i++) {
-        $suma += $i;
-    }
 
-    echo "El resultado de la iteración es $suma";
+
+    if (isset($numero)) {
+        for ($i = 1; $i <= $numero; $i++) {
+            $suma += $i;
+        }
+        echo "El resultado de la iteración es $suma";
+    }else {
+        echo "No puedes dejar el campo numérico null";
+    }
+    
 
 ?>
