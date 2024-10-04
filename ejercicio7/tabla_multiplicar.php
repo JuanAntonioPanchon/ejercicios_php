a2bcf6abf145431b67df37e602b72963d248a5d2
JuanAntonioPanchon <martin.panchon.juan.antonio@iescamas.es>
2024-10-04 08:38:00 +0200

    Add files via upload
    He realizado las comprobaciones de que sea número. Además he añadido la restricción de min y max en la parte del cliente para que no ponga nº menores de 0 y mayores de 10.

--- ejercicio7/tabla_multiplicar.php
+++ ejercicio7/tabla_multiplicar.php
@@ -1,10 +1,16 @@
 <?php
     $numero = $_GET["numero"];
     $multiplicacion = 0;
-    for ($i = 1; $i <= 10; $i++) {
-        $multiplicacion = $i * $numero;
-        echo $i . " x " . $numero . " = " . $multiplicacion . "<br>";
+
+    if($numero>0 && $numero <= 10 && isset($numero)){
+        for ($i = 1; $i <= 10; $i++) {
+            $multiplicacion = $i * $numero;
+            echo $i . " x " . $numero . " = " . $multiplicacion . "<br>";
+        }    
+    }else{
+        print("El número introducido debe ser entre 0 y 10");
     }
 
+    
 
 ?>
